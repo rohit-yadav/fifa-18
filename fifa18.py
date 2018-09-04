1616eb2af06ce13a184190cca7de980a1bbe42ce
rohit <rohityadav2@outlook.com>
2018-09-04 21:12:22 +0530

    Docs: Add introduction

--- fifa18.py
+++ fifa18.py
@@ -1,6 +1,10 @@
 
 # coding: utf-8
 
+# # Introduction
+# 
+# FIFA, also known as FIFA Football or FIFA Soccer, is a series of association football video games or football simulator, released annually by Electronic Arts under the EA Sports label.
+
 # In[1]:
 
 
